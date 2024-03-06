79ceab536c5912cd2f49dbf40b08c237b77c1b0d
Omar ACHKIR <omar0642624692@gmail.com>
2024-03-06 22:42:06 +0000

    6-final-user.js

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,19 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signUpUserPromise = signUpUser(firstName, lastName);
+  const uploadPhotoPromise = uploadPhoto(fileName);
+
+  return Promise.allSettled([signUpUserPromise, uploadPhotoPromise]).then((values) => {
+    const results = [];
+    values.forEach((element) => {
+      if (element.status === 'fulfilled') {
+        results.push({ status: element.status, value: element.value });
+      } else {
+        results.push({ status: element.status, value: `${element.reason}` });
+      }
+    });
+    return results;
+  });
+}
