b9ccace8e8e8107ba35939a87dd9942481325817
Omar <omar0642624692@gmail.com>
2024-04-29 20:45:57 +0000

    0-console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} message - The message to display.
+ * @returns {void}
+ */
+const displayMessage = (message) => {
+  console.log(message);
+};
+
+module.exports = displayMessage;
